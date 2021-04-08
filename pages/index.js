07759b12b423d2511fa9083a04aa85d121bf658a
geenva <geenva@users.noreply.github.com>
2021-04-08 17:12:38 +0800

    feat: switch to function

--- pages/index.js
+++ pages/index.js
@@ -26,50 +26,40 @@ export async function getServerSideProps() {
   }
 }
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: null,
-      load: true,
-    };
-  }
+export default function Home({ data }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Marcus</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <Image
+            className="center"
+            src={data}
+            alt="Profile picture"
+            width="150vw"
+            height="150vw"
+          />
+          <br></br>
+          Marcus Y.
+        </h1>
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Marcus</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            <Image
-              className="center"
-              src={this.props.data}
-              alt="Profile picture"
-              width="150vw"
-              height="150vw"
-            />
-            <br></br>
-            Marcus Y.
-          </h1>
+        <p className={styles.description}>
+          Student-developer working in TypeScript.
+          <br />
+          <Link href="/about">About</Link> · <Link href="/blog">Blog</Link> ·{" "}
+          <Link href="https://github.com/geenva/">GitHub</Link> ·{" "}
+          <Link href="/contact">Contact</Link>
+        </p>
+      </main>
 
-          <p className={styles.description}>
-            Student-developer working in TypeScript.
-            <br />
-            <Link href="/about">About</Link> · <Link href="/blog">Blog</Link> ·{" "}
-            <Link href="https://github.com/geenva/">GitHub</Link> ·{" "}
-            <Link href="/contact">Contact</Link>
-          </p>
-        </main>
-
-        <footer className={styles.footer}>
-          © 2021-present Marcus Y. This website is open-sourced{" "}
-          <a href="https://github.com/geenva/m">  here  </a> under the{" "}
-          <Link href="/LICENSE.txt">  MIT license</Link>.
-        </footer>
-      </div>
-    );
-  }
+      <footer className={styles.footer}>
+        © 2021-present Marcus Y. This website is open-sourced{" "}
+        <a href="https://github.com/geenva/m">  here  </a> under the{" "}
+        <Link href="/LICENSE.txt">  MIT license</Link>.
+      </footer>
+    </div>
+  );
 }
