1aa6b19aa54ce2f31ff8c1a40737032a3e0cc11b
geenva <geenva@users.noreply.github.com>
2021-06-15 22:53:33 +0800

    ⏪ Re-add removed changes

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -13,6 +13,7 @@ export default function Home({
     date: string;
     title: string;
     id: string;
+    topic: string;
   }[];
 }) {
   return (
@@ -20,29 +21,25 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, topic }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                {topic.toUpperCase()} | <Date dateString={date} />
               </small>
             </li>
           ))}
         </ul>
       </section>
+      <Link href="/">
+        <a>← Back to home</a>
+      </Link>
     </Layout>
   );
 }
