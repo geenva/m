44c87a133508c710ecd4bdb318065f7d13b4e85c
geenva <geenva@users.noreply.github.com>
2021-12-30 00:05:58 +0000

    init

--- posts/welcome.md
+++
@@ -1,7 +0,0 @@
----
-title: "Welcome!"
-date: "2021-03-20"
-topic: "misc"
----
-
-Welcome to my blog. Content coming soon.
--- typed/strings.ts
+++
@@ -1,9 +0,0 @@
-const strings = [
-  "Student-developer working in TypeScript",
-  "Student-developer working in Node.js",
-  "Student-developer working in React",
-  "Student-developer working in Next.js",
-  "Student-developer working in JavaScript",
-  "Student-developer learning Rust",
-];
-export default strings;
