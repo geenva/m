f50db402c5620eb57e320b27a992abbdd91eb67b
geenva <geenva@users.noreply.github.com>
2022-01-02 17:20:51 +0000

    create base

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <main className="font-sans bg-kid-a-mnesia bg-cover flex flex-col h-screen text-white">
-      <div className="m-16">
-        <h1 className="text-5xl font-bold">Marcus Y.</h1>
-        <br />
-        <p className="text-2xl">Coming soon...</p>
+    <main className="font-sans bg-black flex flex-col h-screen text-white">
+      <div className="m-12">
+        <h1 className="text-6xl font-bold">Marcus Y.</h1>
+        <p className="opacity-70 text-xl">
+          Software developer currently in the{" "}
+          <span className="font-bold">United Kingdom</span>.
+        </p>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      /*
       fontFamily: {
         sans: `Inter, ui-sans-serif, system-ui, -apple-system,
             BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans",
             sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
       },
+      */
       backgroundImage: {
         "kid-a-mnesia": "url('/img/kid-a-mnesia.jpg')",
       },
