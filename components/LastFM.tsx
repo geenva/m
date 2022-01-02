b1dbb77a2d653a7d7f8d58980b7977ae40576da8
geenva <geenva@users.noreply.github.com>
2022-01-02 18:43:13 +0000

    fix error

--- components/LastFM.tsx
+++ components/LastFM.tsx
@@ -1,10 +1,7 @@
 import { useLastFM } from "use-last-fm";
 
 const LastFM = () => {
-  if (!process.env.LAST_FM_API_KEY)
-    throw new Error("No last.fm API key defined in .env.");
-
-  const lastFM = useLastFM("flarely", process.env.LAST_FM_API_KEY);
+  const lastFM = useLastFM("flarely", process.env.LAST_FM_API_KEY ?? "");
   if (lastFM.status !== "playing") return null;
 
   return (
