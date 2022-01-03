9eed51ccb59d3d66e1dd1d764047a4af25f623c6
geenva <geenva@users.noreply.github.com>
2022-01-03 01:34:20 +0000

    fix status

--- components/Status.tsx
+++ components/Status.tsx
@@ -5,19 +5,18 @@ export default function Status() {
   const { data } = useLanyard(process.env.NEXT_PUBLIC_DISCORD_ID!);
   const activityData = data?.activities;
   let activity = null;
+  let status = data?.discord_status == "offline" ? "Offline" : "Online";
 
   activityData?.forEach((item) => {
     if (item.name !== "Spotify" && item.name !== "Apple Music")
       activity = "Playing " + item.name + " • " + item.details;
-
-    if (data?.discord_status == "offline") return null;
   });
 
   return (
     <p className="flex space-x-2 -space-y-1 opacity-80">
       <IoMdGitCommit />{" "}
       <span>
-        Online{" "}
+        {status}{" "}
         {activity ? (
           <>
             | <span className="animate-pulse">{activity}</span>
