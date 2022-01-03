135f92d3934b2f0b793869d97c3e1a949883c647
geenva <geenva@users.noreply.github.com>
2022-01-03 14:32:30 +0000

    add columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,42 @@
 import type { NextPage } from "next";
 import Song from "../components/Song";
 import Status from "../components/Status";
+import Link from "next/link";
+import { IoInformationCircle, IoPencil } from "react-icons/io5";
 
 const Home: NextPage = () => {
   return (
     <main className="font-sans bg-black flex flex-col h-screen text-white">
-      <div className="m-12">
-        <h1 className="text-6xl font-bold">Marcus Y.</h1>
-        <p className="opacity-70 text-xl">
-          Software developer currently in the{" "}
-          <span className="font-bold">United Kingdom</span>.
-        </p>
-        <br />
-        <Status />
-        <Song />
-        <br />
-        <p className="text-2xl">Coming soon.</p>
+      <div className="m-12 grid grid-cols-none lg:grid-cols-2">
+        <div>
+          <h1 className="text-6xl font-bold">Marcus Y.</h1>
+          <p className="opacity-70 text-xl">
+            Software developer currently in the{" "}
+            <span className="font-bold">United Kingdom</span>.
+          </p>
+          <br />
+          <Status />
+          <Song />
+          <br />
+          <div className="text-2xl">
+            <Link href="/about">
+              <a className="flex space-x-2 -space-y-1 hover:underline">
+                <IoInformationCircle />
+                <span>About</span>
+              </a>
+            </Link>
+            <Link href="/blog">
+              <a className="flex space-x-2 -space-y-1 hover:underline">
+                <IoPencil />
+                <span>Blog</span>
+              </a>
+            </Link>
+          </div>
+          <br />
+        </div>
+        <div className="text-2xl md:text-4xl">
+          <p>Hello! Welcome to my website. Something something something.</p>
+        </div>
       </div>
     </main>
   );
