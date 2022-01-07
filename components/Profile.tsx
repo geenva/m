c17f5dd8db7ff909d8511f2899aa933ea8e5620d
geenva <geenva@users.noreply.github.com>
2022-01-07 18:39:59 +0000

    add lanyard placeholder

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,14 +1,25 @@
 import { Data, LanyardError } from "use-lanyard";
 import Image from "next/image";
 import { SWRResponse } from "swr";
+import placeholder from "../public/img/low-quality-profile-picture.png";
 
 export default function Status(props: {
   lanyard: SWRResponse<Data, LanyardError>;
 }) {
   const { data } = props.lanyard;
-  if (!data?.discord_user.avatar) return null;
+  if (!data?.discord_user.avatar)
+    return (
+      <Image
+        className="rounded-full"
+        src={placeholder}
+        layout="intrinsic"
+        width="160"
+        height="160"
+        alt="Profile picture"
+      />
+    );
   const avatar =
-    "https://dcdn.dstn.to/avatars/" + data?.discord_user.id + "?size=1280";
+    "https://dcdn.dstn.to/avatars/" + data?.discord_user.id + "?size=512";
 
   return (
     <Image
